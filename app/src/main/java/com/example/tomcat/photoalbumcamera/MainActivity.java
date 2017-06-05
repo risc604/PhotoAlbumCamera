691c174b2c425137e64d08f23503a1af798961ca
tomcat <tomcat.cheng@microlife.com.tw>
2017-06-05 15:08:44 +0800

    add permission

--- app/src/main/java/com/example/tomcat/photoalbumcamera/MainActivity.java
+++ app/src/main/java/com/example/tomcat/photoalbumcamera/MainActivity.java
@@ -1,13 +1,98 @@
 package com.example.tomcat.photoalbumcamera;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity
+{
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private final int REQUEST_CAMERA = 1;
+
+    ImageView   imgView;
+    TextView    txtView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        Log.d(TAG, "onCreate()...");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+        initControl();
     }
+
+    @Override
+    protected void onResume()
+    {
+        Log.d(TAG, "onResume()...");
+        super.onResume();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        Log.d(TAG, "onActivityResult()...");
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults)
+    {
+        Log.d(TAG, "onRequestPermissionsResult()...");
+
+        switch (requestCode)
+        {
+            case REQUEST_CAMERA:
+            {
+                if ((grantResults.length > 0) &&
+                        (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                {
+                    Toast.makeText(getApplicationContext(), "Permission granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "Permission denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    private void initView()
+    {
+        Log.d(TAG, "initView()...");
+
+        imgView = (ImageView) findViewById(R.id.imageView);
+        txtView = (TextView) findViewById(R.id.textView);
+    }
+
+    private void initControl()
+    {
+        Log.d(TAG, "initControl()...");
+
+        ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.CAMERA,
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                this.REQUEST_CAMERA);
+    }
+
 }
